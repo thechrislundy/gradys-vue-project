c7b766119ffcac78dcc12fd00c6c08ab7833dd29
thechrislundy <thechrislundy@gmail.com>
2024-06-13 08:59:58 -0600

    added icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="text-center">
-    <h1>{{ msg }}</h1>
+    <h1>
+      <i-mdi-human-greeting-variant style="padding-right: 10px" class="text-black-50" />
+      {{ msg }}
+    </h1>
     <p>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
