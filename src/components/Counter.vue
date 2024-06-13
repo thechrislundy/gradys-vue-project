1a4cba5d56508f16f4d57466b4992733ecd59b45
thechrislundy <thechrislundy@gmail.com>
2024-06-13 12:17:31 -0600

    changed counter columns

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,13 +3,13 @@
     <h2 class="card-header">Counter Example</h2>
     <div class="card-body">
       <div class="row my-4">
-        <div class="col-3">
+        <div class="col-sm-6 col-lg-3 my-2">
           <button @click="add" class="btn btn-primary">Add to count</button>
         </div>
-        <div class="col-3">
+        <div class="col-sm-6 col-lg-3 my-2">
           <button @click="subtract" class="btn btn-primary">Subtract to count</button>
         </div>
-        <div class="col-3">
+        <div class="col-sm-6 col-lg-3 my-2">
           <div class="input-group">
             <input type="number" class="form-control" v-model="amount" />
             <button @click="addAmount" class="btn btn-primary" type="button" id="button-addon2">
@@ -17,7 +17,7 @@
             </button>
           </div>
         </div>
-        <div class="col-3">
+        <div class="col-sm-6 col-lg-3 my-2">
           <button @click="count = 0" class="btn btn-secondary">Reset count</button>
         </div>
       </div>
